82feaf645e9541fd8b861649f9acda5803fef0d3
Indutnj <122307911+Indutnj@users.noreply.github.com>
2023-12-05 11:11:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI , {useUnifiedTopology: true , useNewUrlParser: true});
 //mongodb+srv://root:<password>@cluster0.dwtynnw.mongodb.net/
-//mongodb+srv://sathya:sathyapr@cluster0.dkuc0.mongodb.net/sheygram
+
 const connection = mongoose.connection
 
 connection.on('connected' , ()=>{
